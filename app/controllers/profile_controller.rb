f9e1cf24129b413af6d621d619d40cd27cb4c739
igor <zawgorodskiy.2012@gmail.com>
2017-12-14 22:28:51 +0200

    fixed some errors

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,21 +1,2 @@
 class ProfileController < ApplicationController
-  before_action :find_profile, only: %i[show update edit]
-
-  def update
-    if @profile.update(profile_params)
-      redirect_to user_profile_path(current_user)
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def find_profile
-    @profile = User.find(params[:id]).profile
-  end
-
-  def profile_params
-    params.require(:profile).permit(:avatar, :first_name, :last_name, :age)
-  end
 end
